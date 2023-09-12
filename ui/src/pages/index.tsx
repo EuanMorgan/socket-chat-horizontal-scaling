dbd5c56ba5a10e3c9db650383985c6409c71c577
Euan Morgan <euanmorgan48@gmail.com>
2023-09-12 19:38:36 +0100

    ui tweaks

--- ui/src/pages/index.tsx
+++ ui/src/pages/index.tsx
@@ -1,7 +1,7 @@
 import {Button} from '@/components/ui/button';
 import {Textarea} from '@/components/ui/textarea';
 import {CHANNELS} from '@/constants/events';
-import {FormEvent, useEffect, useState} from 'react';
+import React, {FormEvent, useEffect, useState} from 'react';
 import io, {type Socket} from 'socket.io-client';
 
 type Message = {
@@ -39,14 +39,32 @@ export default function Home() {
 
   const [messages, setMessages] = useState<Message[]>([]);
 
+  const [connectionCount, setConnectionCount] = useState<number>(0);
+
+  const scrollToBottom = () => {
+    if (listRef.current) {
+      listRef.current.scrollTop = listRef.current.scrollHeight + 1000;
+    }
+  };
+
   useEffect(() => {
     socket?.on('connect', () => {
       console.log('connected');
     });
 
     socket?.on(CHANNELS.NEW_MESSAGE_CHANNEL, (message: Message) => {
-      setMessages(prev => [message, ...prev]);
+      setMessages(prev => [...prev, message]);
+      setTimeout(() => {
+        scrollToBottom();
+      }, 0);
     });
+
+    socket?.on(
+      CHANNELS.CONNECTION_COUNT_UPDATED_CHANNEL,
+      ({count}: {count: number}) => {
+        setConnectionCount(count);
+      }
+    );
   }, [socket]);
 
   const handleSubmit = async (e: FormEvent) => {
@@ -59,11 +77,22 @@ export default function Home() {
     });
   };
 
+  const listRef = React.useRef<HTMLOListElement>(null);
+
   return (
-    <main className='flex flex-col p-4 w-full max-w-3xl mx-auto'>
-      {messages.map(m => (
-        <li key={m.id}>{m.message}</li>
-      ))}
+    <main className='flex flex-col p-4 w-full max-w-3xl mx-auto h-full'>
+      <h1 className='text-4xl font-bold text-center mb-4'>
+        Chat ({connectionCount})
+      </h1>
+      <ol ref={listRef} className='flex-1 overflow-y-scroll overflow-x-hidden'>
+        {messages.map(m => (
+          <li className='bg-gray-100 rounded-lg p-4 my-2 break-all' key={m.id}>
+            <p className='text-sm text-gray-500'>{m.createdAt}</p>
+            <p className='text-sm text-gray-500'>{m.port}</p>
+            <p>{m.message}</p>
+          </li>
+        ))}
+      </ol>
       <form onSubmit={handleSubmit} className='flex items-center '>
         <Textarea
           className='rounded-lg mr-4'
